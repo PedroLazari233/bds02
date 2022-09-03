9dc533874fb3207567e5e89e2e606e289ec56c3e
Pedro Lazari <67986647+PedroLazari233@users.noreply.github.com>
2022-09-03 12:34:29 -0300

    Layers implemented to attend test criterion

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,43 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional
+    public EventDTO updateEvent(Long id, EventDTO eventDTO) {
+        try{
+            Event event = eventRepository.getOne(id);
+            copyDtoToEntity(eventDTO, event);
+            event = eventRepository.save(event);
+            return new EventDTO(event);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    private void copyDtoToEntity(EventDTO eventDTO, Event event){
+
+        event.setName(eventDTO.getName());
+        event.setDate(eventDTO.getDate());
+        event.setUrl(eventDTO.getUrl());
+        event.setCity(cityRepository.getOne(eventDTO.getCityId()));
+    }
+}
