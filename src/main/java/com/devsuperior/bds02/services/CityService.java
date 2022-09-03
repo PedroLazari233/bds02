2c4946badc121b4e79612a9fe954f7136a4565da
Pedro Lazari <67986647+PedroLazari233@users.noreply.github.com>
2022-09-03 12:56:12 -0300

    Insert new city implemented in order to attend test criterion

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -6,6 +6,7 @@ import com.devsuperior.bds02.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +18,16 @@ public class CityService {
     @Autowired
     private CityRepository cityRepository;
 
+    @Transactional(readOnly = true)
     public List<CityDTO> findAll() {
         List<City> cities = cityRepository.findAll(Sort.by("name"));
         return cities.stream().map(city -> new CityDTO(city)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insertCity(CityDTO cityDTO) {
+        City city = new City(cityDTO.getId(), cityDTO.getName());
+        city = cityRepository.save(city);
+        return new CityDTO(city);
     }
 }
